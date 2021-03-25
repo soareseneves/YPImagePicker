5255972de811cb914bc5f8829423b9e8ae40c550
Soares e Neves <soareseneves@terra.com.br>
2021-03-25 12:32:35 -0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "YPImagePicker",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS("8.0")
+    ],
+    products: [
+        .library(name: "YPImagePicker", targets: ["YPImagePicker"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/freshOS/Stevia", from: "4.7.3"),
+        .package(url: "https://github.com/HHK1/PryntTrimmerView", from: "4.0.2")
+    ],
+    targets: [
+        .target(
+            name: "YPImagePicker",
+            path: "YPImagePicker",
+            exclude: [
+                "Info.plist",
+            ],
+            resources: [.process("Resources")]
+        )
+    ]
+)
