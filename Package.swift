e0c2a4c4ae1de3ce1a7516861672dde34a204182
Soares e Neves <soareseneves@terra.com.br>
2021-03-25 15:23:03 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "YPImagePicker",
     defaultLocalization: "en",
     platforms: [
-        .iOS("8.0")
+        .iOS("9.0")
     ],
     products: [
         .library(name: "YPImagePicker", targets: ["YPImagePicker"])
