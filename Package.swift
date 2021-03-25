956fccf95b702c2cc2b7ea7e51b4704728acacf0
Soares e Neves <soareseneves@terra.com.br>
2021-03-25 14:55:28 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .target(
             name: "YPImagePicker",
-            path: "YPImagePicker",
+            path: "Source",
             exclude: [
                 "Info.plist",
             ],
